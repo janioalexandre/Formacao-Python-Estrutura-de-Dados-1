fe03df569cf7b648cc29fac2c1f295406f224396
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-07-10 23:58:35 -0300

    Vetor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from array import array
+from vetores import vetor
 
 print("Hello, World!")
 
@@ -11,3 +12,13 @@ vetores_inteiros.insert(3, 4)
 print(vetores_inteiros)
 
 print(vetores_inteiros.index(2))
+
+print(30 * "-", "Menu", 30 * "-")
+print("1. Vetores")
+print("2. Listas Ligadas")
+
+menu = int(input("Digite a opção desejada: "))
+
+if menu == 1: 
+    vetor_teste = vetor.Vetor(3)
+    vetor_teste.inserir_elemento_posicao(1, 0)
--- vetores/vetor.py
+++ vetores/vetor.py
@@ -1,6 +1,7 @@
 class Vetor:
     def __init__(self, tamanho):
-        self.__tamanhjo = tamanho
-        self.__elementos = None * tamanho
+        self.__tamanho = tamanho
+        self.__elementos = [None] * tamanho
 
-        
+    def inserir_elemento_posicao(self, elemento, posicao):
+        self.__elementos[posicao] = elemento
