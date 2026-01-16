cedc464e9a8eabbf8d56b34cdfddb4cbfcf996ae
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2026-01-15 22:25:57 -0300

    Inserindo elementos

--- main.py
+++ main.py
@@ -21,4 +21,11 @@ menu = int(input("Digite a opção desejada: "))
 
 if menu == 1: 
     vetor_teste = vetor.Vetor(3)
-    vetor_teste.inserir_elemento_posicao(1, 0)
+    # vetor_teste.inserir_elemento_posicao(1, 0)
+
+    vetor_teste.inserir_elemento_final(1)
+    vetor_teste.inserir_elemento_final(2)
+    
+    print(vetor_teste.listar_elementos(0))
+    print(vetor_teste.listar_elementos(1))
+    
